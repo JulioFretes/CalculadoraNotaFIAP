d55b06280495c9e1921788d289ff11916cced7aa
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-25 19:59:45 -0300

    Screen update

---
+++ CalculadoraNotaFIAP/Models/Materia.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CalculadoraNotaFIAP.Models
+{
+    public class Materia
+    {
+        [Display(Name = "Matéria")]
+        public NomeMateria Nome { get; set; }
+
+        public virtual ICollection<Nota> Nota { get; set; }
+
+    }
+    public enum NomeMateria
+    {
+        [Display(Name = "COMPLIANCE & QUALITY ASSURANCE")]
+        Compliance,
+
+        [Display(Name = "DATABASE APPLICATION & DATA SCIENCE")]
+        Database,
+
+        [Display(Name = "DEVOPS TOOLS & CLOUD COMPUTING")]
+        Devops,
+
+        [Display(Name = "DIGITAL BUSINESS ENABLEMENT")]
+        Digital,
+
+        [Display(Name = "DISRUPTIVE ARCHITECTURES: IT, IOB & IA")]
+        IOT,
+
+        [Display(Name = "ENTERPRISE APPLICATION DEVELOPMENT")]
+        Enterprise,
+
+        [Display(Name = "HYBRID MOBILE APP DEVELOPMENT")]
+        Hybrid
+    }
+}
--- CalculadoraNotaFIAP/Models/Nota.cs
+++ CalculadoraNotaFIAP/Models/Nota.cs
@@ -8,6 +8,7 @@ namespace CalculadoraNotaFIAP.Models
     {
         [Key]
         public int Id { get; set; }
+
         [Display(Name = "Checkpoint 1")]
         public Decimal Cp1 { get; set; }
         [Display(Name = "Checkpoint 2")]
@@ -24,13 +25,7 @@ namespace CalculadoraNotaFIAP.Models
         public Decimal GlobalSolution { get; set; }
 
         [Display(Name = "Media do primeiro semestre")]
-        public Decimal NotaPrimeiroSemestre { get; set; }
-
-        [Display(Name = "Semestre do ano")]
-        public SemestreAno Semestre { get; set; }
-
-        [Display(Name = "Matéria")]
-        public Materia Materia { get; set; }
+        public Decimal NotaPrimeiroSemestre { get; set; }        
 
         [Display(Name = "Calcular nota mínima")]
         public bool Calcular { get; set; }
@@ -43,34 +38,5 @@ namespace CalculadoraNotaFIAP.Models
 
         [ForeignKey("UserId")]
         public IdentityUser User { get; set; }
-    }
-
-    public enum SemestreAno
-    {
-        Primeiro, Segundo
-    }
-
-    public enum Materia
-    {
-        [Display(Name = "COMPLIANCE & QUALITY ASSURANCE")]
-        Compliance,
-
-        [Display(Name = "DATABASE APPLICATION & DATA SCIENCE")]
-        Database,
-
-        [Display(Name = "DEVOPS TOOLS & CLOUD COMPUTING")]
-        Devops,
-
-        [Display(Name = "DIGITAL BUSINESS ENABLEMENT")]
-        Digital,
-
-        [Display(Name = "DISRUPTIVE ARCHITECTURES: IT, IOB & IA")]
-        IOT,
-
-        [Display(Name = "ENTERPRISE APPLICATION DEVELOPMENT")]
-        Enterprise,
-
-        [Display(Name = "HYBRID MOBILE APP DEVELOPMENT")]
-        Hybrid
-    }
+    }    
 }
